d8993e0008ee17405e1c726ac810b5615f5f7b84
towerofpower256 <towerofpower256@gmail.com>
2018-04-24 16:03:02 +1000

    Update Format DateTime.js

--- Format DateTime.js
+++ Format DateTime.js
@@ -1,16 +1,15 @@
 // Date Time Format guide: https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
 
-// ===== Vars
-D3U.INTERNAL_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-D3U.INTERNAL_DATETIME_TIMEZONE = "GMT";
-
 /*
-	Outputs a GlideDateTime in the given arbitrary format
-	Att current, ServiceNow is not able to do this using ServiceNow functions,
-		and this function taps into the Java-native classes
+	Converts a date-time string from one format / timezone to another.
+	At current, ServiceNow is not able to do this using ServiceNow functions,
+		this function taps into the Java-native functionality.
 */
 D3U.formatDateTimeString = function(inputValue, inputFormat, inputTzName, outputFormat, outputTzName) {
-	if (JSUtil.nil(inputTzName)) inputTzName = D3U.INTERNAL_DATETIME_FORMAT; //Default to the system's timezone, if not specified
+	var INTERNAL_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	
+	
+	if (JSUtil.nil(inputTzName)) inputTzName = INTERNAL_DATETIME_FORMAT; //Default to the system's timezone, if not specified
 	if (JSUtil.nil(outputTzName)) outputTzName = gs.getSession().getTimeZoneName(); //Default to the user's timezone, if not specified
 	
 	//Configure input converter
@@ -28,6 +27,12 @@ D3U.formatDateTimeString = function(inputValue, inputFormat, inputTzName, output
 	return r;
 };
 
+/*
+	Returns the string value of a GlideDateTime object in the specified format and timezone
+	Same as formateDateTimeString, but works with GlideDateTime objects and requires fewer para.meters.
+*/
 D3U.formatDateTimeGDT = function(gdt, outputFormat, tzName) {
-	return D3U.formatDateTimeString(gdt.getValue(), this.INTERNAL_DATETIME_FORMAT, D3U.INTERNAL_DATETIME_TIMEZONE, outputFormat, tzName);
+	var INTERNAL_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	var INTERNAL_DATETIME_TIMEZONE = "GMT";
+	return D3U.formatDateTimeString(gdt.getValue(), INTERNAL_DATETIME_FORMAT, INTERNAL_DATETIME_TIMEZONE, outputFormat, tzName);
 };
