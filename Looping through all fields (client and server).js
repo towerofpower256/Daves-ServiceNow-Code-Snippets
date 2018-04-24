717b177f2908f8914d65b3a9319972629c4bb602
towerofpower256 <towerofpower256@gmail.com>
2018-04-24 16:06:17 +1000

    Create Looping through all fields (client and server).js

---
+++ Looping through all fields (client and server).js
@@ -0,0 +1,10 @@
+// SERVER
+
+
+
+// CLIENT
+var fields = g_form.getEditableFields();  
+for (var x = 0; x < fields.length; x++) {  
+    var field = fields[x];
+    // Do whatever you want to do to the fields in here 
+}
