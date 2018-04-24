7909e3673a04c84bf226a8543c9d402ab4b840db
towerofpower256 <towerofpower256@gmail.com>
2018-04-24 16:08:56 +1000

    Update Looping through all fields (client and server).js

--- Looping through all fields (client and server).js
+++ Looping through all fields (client and server).js
@@ -1,4 +1,10 @@
 // SERVER
+// Assuming that you've got a GlideRecord in a variable called 'gr'
+var fields = gr.getFields();
+for (var i = 0; i < fields.size(); i++) {
+    var field = fields.get(i);
+    // Do whatever you want to do to the field in here
+}
 
 
 
@@ -6,5 +12,5 @@
 var fields = g_form.getEditableFields();  
 for (var x = 0; x < fields.length; x++) {  
     var field = fields[x];
-    // Do whatever you want to do to the fields in here 
+    // Do whatever you want to do to the field in here 
 }
