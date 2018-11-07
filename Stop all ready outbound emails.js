870192cf2f0d92af4c3df89a2fe608f8e815e842
towerofpower256 <towerofpower256@gmail.com>
2018-11-08 08:30:11 +1000

    Update Stop all ready outbound emails.js

--- Stop all ready outbound emails.js
+++ Stop all ready outbound emails.js
@@ -1,6 +1,8 @@
 // Use this script to ignore any emails that are ready to be sent
 // Useful when activating email sending from a sub-production instance,
 //	and there's like a bazillion emails waiting to be sent.
+// Also useful after a clone and you want to clear out all of the emails
+//	in the queue that are waiting to be sent.
 
 var grEmail = new GlideRecord("sys_email");
 grEmail.addQuery("type", "send-ready");
